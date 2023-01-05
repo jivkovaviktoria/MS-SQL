80814c1f83d16068ae4b9fff751e4b38aa58ac35
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-05 20:49:34 +0200

    Solved "02. Create tables" from Database Introduction

--- 1.DatabaseIntroduction/DatabaseIntroduction.sql
+++ 1.DatabaseIntroduction/DatabaseIntroduction.sql
@@ -1,3 +1,18 @@
 --Create database (Problem 1)
 create database Minions
 use Minions
+
+
+--Create tables (Problem 2)
+create table Minions(
+    Id int not null,
+    Name nvarchar(50),
+    Age int
+	primary key(Id)
+)
+
+create table Towns(
+    Id int not null,
+    Name nvarchar(50) not null,
+	primary key(Id)
+)
