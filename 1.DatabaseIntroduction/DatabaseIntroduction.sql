58b5cc83246eeb73366b435f0682f4d05f38f703
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-05 20:50:37 +0200

    Solved "04. Insert records in both tables" from Database Introduction

--- 1.DatabaseIntroduction/DatabaseIntroduction.sql
+++ 1.DatabaseIntroduction/DatabaseIntroduction.sql
@@ -19,3 +19,7 @@ create table Towns(
 
 --Alter Minions table (Problem 3)
 alter table Minions add foreign key(TownId) references Towns(Id)
+
+--Insert records in both tables (Problem 4)
+insert into Towns(id, name) values (1, 'Sofia'), (2, 'Plovdiv'), (3, 'Varna')
+insert into Minions(id, name, age, TownId) values (1, 'Kevin', 22, 1), (2, 'Bob', 15, 3), (3, 'Steward', null, 2)
