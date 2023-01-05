a3ff9f0a4150e2ed51253672814f0ca8c667ecae
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-05 20:51:12 +0200

    Solved "05. Truncate Table Minions" from Database Introduction

--- 1.DatabaseIntroduction/DatabaseIntroduction.sql
+++ 1.DatabaseIntroduction/DatabaseIntroduction.sql
@@ -23,3 +23,6 @@ alter table Minions add foreign key(TownId) references Towns(Id)
 --Insert records in both tables (Problem 4)
 insert into Towns(id, name) values (1, 'Sofia'), (2, 'Plovdiv'), (3, 'Varna')
 insert into Minions(id, name, age, TownId) values (1, 'Kevin', 22, 1), (2, 'Bob', 15, 3), (3, 'Steward', null, 2)
+
+--Truncate table Minions (Problem 5)
+delete from Minions
