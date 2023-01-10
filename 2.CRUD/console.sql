405880b2157b766daa6781b3114999f2a1ed77a3
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-10 10:27:28 +0200

    Solved "Find salary of each Employee" from CRUD

--- 2.CRUD/console.sql
+++ 2.CRUD/console.sql
@@ -3,3 +3,6 @@ select * from Departments;
 
 -- 03. Find all Departments names
 select Name from Departments;
+
+--04. Find Salary of each Employee
+select FirstName, LastName, Salary from Employees;
