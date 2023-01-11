284d96479056b3c2c7f1249034bd9f37db3d019b
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-11 11:37:35 +0200

    Solved "Employees with salary above" from Functions and stored procedures

--- 7.FunctionsAndStoredProcedures/FunctionsAndStoredProcedures.sql
+++ 7.FunctionsAndStoredProcedures/FunctionsAndStoredProcedures.sql
@@ -2,6 +2,11 @@
 create proc usp_GetEmployeesSalaryAbove35000
 as select e.FirstName, e.LastName from Employees as e where e.Salary > 35000
 
+--2 Employees with salary above
+create proc usp_GetEmployeesSalaryAboveNumber
+@num decimal(18,4)
+as select e.FirstName, e.LastName from Employees as e where e.Salary >= @num;
+
 --3. Town names starting with
 create proc usp_GetTownsStartingWith
 @prefix nvarchar(max)
