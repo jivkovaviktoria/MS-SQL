1a060a03301bbb65aafc79e21773f185882fbf5e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-11 11:10:51 +0200

    Solved "Addresses with Towns" from Subqueries and Joins

--- 5.SubqueriesAndJoins/SubqueriesAndJoins.sql
+++ 5.SubqueriesAndJoins/SubqueriesAndJoins.sql
@@ -2,3 +2,12 @@
 select top 5 e.EmployeeID, e.JobTitle, a.AddressID, a.AddressText from Employees as e
 inner join Addresses as a on e.AddressID = a.AddressID
 order by a.AddressID
+
+--2. Addresses with Towns
+select top 50 e.FirstName, e.LastName, t.Name as Town, a.AddressText
+from Employees as e
+inner join Addresses as a
+    on e.AddressID = a.AddressID
+inner join Towns as t
+ on a.TownID = t.TownID
+order by e.FirstName, e.LastName
