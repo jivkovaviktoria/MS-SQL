bdaafded9fce46d1a0a537e067be5f51ffee7cee
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-11 11:38:05 +0200

    Solved "Employees from town" from Functions and stored procedures

--- 7.FunctionsAndStoredProcedures/FunctionsAndStoredProcedures.sql
+++ 7.FunctionsAndStoredProcedures/FunctionsAndStoredProcedures.sql
@@ -11,3 +11,11 @@ as select e.FirstName, e.LastName from Employees as e where e.Salary >= @num;
 create proc usp_GetTownsStartingWith
 @prefix nvarchar(max)
 as select t.Name from Towns as t where t.Name like @prefix + '%'
+
+--4. Employees from town
+create or alter proc usp_GetEmployeesFromTown
+@town nvarchar(max)
+as select e.FirstName, e.LastName from Employees as e
+join Addresses a on e.AddressID = a.AddressID
+join Towns as t on a.TownID = t.TownID
+where t.Name = @town
