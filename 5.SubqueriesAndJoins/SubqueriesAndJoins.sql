8d423e7bb239507c0dd3583416cc1866a92aa5dc
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-11 11:11:56 +0200

    Solved "Employees departments" from Subqueries and Joins

--- 5.SubqueriesAndJoins/SubqueriesAndJoins.sql
+++ 5.SubqueriesAndJoins/SubqueriesAndJoins.sql
@@ -19,3 +19,10 @@ inner join Departments as d
 on e.DepartmentID = d.DepartmentID
 where d.Name = 'Sales'
 order by e.EmployeeID
+
+--4. Employees departments
+select top 5 e.EmployeeID, e.FirstName, e.Salary, d.Name from  Employees as e
+inner join Departments as d
+on e.DepartmentID = d.DepartmentID
+where e.Salary > 15000
+order by d.DepartmentID
