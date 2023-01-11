37eae30c554d45a949027bc481e24ca47211b599
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-11 11:36:52 +0200

    Solved "Town names starting with" from Functions and stored procedures

--- 7.FunctionsAndStoredProcedures/FunctionsAndStoredProcedures.sql
+++ 7.FunctionsAndStoredProcedures/FunctionsAndStoredProcedures.sql
@@ -1,3 +1,8 @@
 --1. Employees with salaries above 35000
 create proc usp_GetEmployeesSalaryAbove35000
 as select e.FirstName, e.LastName from Employees as e where e.Salary > 35000
+
+--3. Town names starting with
+create proc usp_GetTownsStartingWith
+@prefix nvarchar(max)
+as select t.Name from Towns as t where t.Name like @prefix + '%'
