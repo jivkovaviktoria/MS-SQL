4917f1a2aed7662883e78205aef25513efc694f7
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-11 10:58:09 +0200

    Solved "Find names of all employees by last name" from Built-in-functions

--- 4.Built-in-functions/Built-in-functions.sql
+++ 4.Built-in-functions/Built-in-functions.sql
@@ -1,2 +1,5 @@
 -- Problem 1. Find Names of All Employees by First Name
 select FirstName, LastName from Employees where FirstName like 'Sa%';
+
+-- Problem 2. Find Names of All employees by Last Name
+select FirstName, LastName from Employees where LastName like '%ei%';
