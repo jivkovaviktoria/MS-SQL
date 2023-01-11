dfd958afc7233415e774a03c39eb93d0702360d6
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-11 11:28:34 +0200

    Solved "Longest magic wand per deposit groups" from Indices and data aggregation

--- 6.IndicesAndDataAggregation/IndicesAndDataAggregation.sql
+++ 6.IndicesAndDataAggregation/IndicesAndDataAggregation.sql
@@ -3,3 +3,7 @@ select count(*) from WizzardDeposits
 
 --2. Longest magic wand
 select max(MagicWandSize) as LongestMagicWand from WizzardDeposits
+
+--3. Longest magic wand per deposit groups
+select DepositGroup, max(MagicWandSize) as LongestMagicWand from WizzardDeposits
+group by DepositGroup
